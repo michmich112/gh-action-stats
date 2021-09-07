a69181ea63cbcf2af9397430bd9a72f6ff508868
michmich112 <cantacuzene.michel@gmail.com>
2021-09-06 21:46:11 -0700

    Working action for tracking action runs

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -27,5 +27,7 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    "max-len": ["warn", {"code": 120, "tabWidth": 2}],
+    "indent": ["error", 2],
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,28 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp();
+
+export const newActionRun = functions.https
+  .onRequest(async (req: functions.Request, res: functions.Response) => {
+    const {method, body, ip} = req;
+    if (method !== "POST") {
+      res.status(405);
+      res.end();
+      return;
+    }
+    const data = {
+      ip,
+      ...body,
+      timestamp: new Date().toISOString(),
+    };
+    try {
+      await admin.firestore().collection("runs").add(data);
+      res.status(200);
+    } catch (e) {
+      console.error("Error saving run to firestore.", e);
+      res.status(500);
+    }
+    res.end();
+  });
