663a486b8fc5b6832decdb65af4bbd8c073f2e9e
michmich112 <cantacuzene.michel@gmail.com>
2021-09-13 11:20:11 -0700

    isGithubActionsAdderess helper function

--- functions/src/__test__/utils/githubUtils.test.ts
+++ functions/src/__test__/utils/githubUtils.test.ts
@@ -1,59 +1,69 @@
-import { isIpInCIDR } from "../../utils/githubUtils";
+import { GithubMeta, isGithubActionsAddress } from "../../utils/githubUtils";
 
-describe("isIpInCIDR", () => {
-  test("[IPv4] it should return true if ip is in CIDR block", () => {
-    expect(isIpInCIDR("192.168.0.1", "192.168.0.0/31")).toBe(true);
-    expect(isIpInCIDR("192.168.0.0", "192.168.0.0/31")).toBe(true);
-    expect(isIpInCIDR("192.168.0.0", "192.168.0.0/24")).toBe(true);
-    expect(isIpInCIDR("192.168.0.0", "192.168.0.0/16")).toBe(true);
-    expect(isIpInCIDR("192.168.0.0", "192.168.0.0/8")).toBe(true);
-    expect(isIpInCIDR("1.2.3.4", "0.0.0.0/0")).toBe(true);
-    expect(isIpInCIDR("1.2.3.4", "1.2.3.4/32")).toBe(true);
-  });
-
-  test("[IPv4] it should return false if an incorrect IPv4 CIDR block is passed", () => {
-    expect(isIpInCIDR("1.2.3.4", "1.2.3.4/33")).toBe(false);
-    expect(isIpInCIDR("252.252.252.252", "253.0.0.0/8")).toBe(false);
-  });
-
-  test("[IPv4] it should return false if ip is not in CIDR block", () => {
-    const ip1 = "192.168.0.2";
-    const ip2 = "192.168.1.1";
-    const cidr = "192.168.0.0/31";
-    expect(isIpInCIDR(ip1, cidr)).toBe(false);
-    expect(isIpInCIDR(ip2, cidr)).toBe(false);
-  });
-
-  test("[IPv6] it should return true if ip is in CIDR block", () => {
-    const cidr = "2603:1030:401:160::/61";
-    expect(isIpInCIDR("2603:1030:401:160:1:1:1:1", cidr)).toBe(true);
-    expect(isIpInCIDR("2603:1030:401:160:1111:1111:1111:1111", cidr)).toBe(true);
-    expect(isIpInCIDR("1111:2222:3333:4444:5555:6666:7777:8888", "1111:2222:3333:4444:5555:6666:7777:8888/128")).toBe(true);
-    expect(isIpInCIDR("1:2:3:4:5:6:7:8", "::/0")).toBe(true);
-  });
-
-  test("[IPv6] it should return false if an incorrect IPv6 CIDR block is passed", () => {
-    expect(isIpInCIDR("1:2:3:4:5:6:7:8", "2603:1030:401:160::/129")).toBe(false);
-    expect(isIpInCIDR("1:2:3:4:5:6:7:8", ":/0")).toBe(false);
-  });
-
-  test("[IPv6] it should return false if ip is not in CIDR block", () => {
-    expect(isIpInCIDR("1:2:3:4:5:6:7:8", "1:3:4:5:6:7:8:9/32")).toBe(false)
-  });
-
-  test("it should return false if comparing a IPv4 Address to an IPv6 CIDR block", () => {
-    expect(false).toBe(true);
-  });
-
-  test("it should return false if comparing a IPv6 Address to an IPv4 CIDR block", () => {
-    expect(false).toBe(true);
-  });
-})
+jest.mock('axios');
+const axios = require('axios');
 
 describe("isGithubActionsAddress", () => {
-  // TODO: mock api call
-  test("it sohuld return true if it is in one of the github actions cidr block", () => {
-    expect(false).toBe(true);
+
+  const githubMeta: GithubMeta = {
+    verifiable_password_authentication: false,
+    ssh_key_fingerprints: {
+      SHA256_RSA: "",
+      SHA256_DSA: "",
+    },
+    hooks: [],
+    web: [],
+    api: [],
+    git: [],
+    packages: [],
+    pages: [],
+    importer: [],
+    actions: [
+      "1.2.3.4/32",
+      "5.6.7.0/24",
+      "1:2:3:4:5:6:7:8/128",
+      "9:10:11:12:13::/80"
+    ],
+    dependabot: [],
+  }
+
+  axios.get.mockImplementation(async (url: string) => {
+    if (url === 'https://api.github.com/meta') {
+      return {
+        data: JSON.stringify(githubMeta)
+      };
+    }
+    return "";
+  })
+
+  test("it should return true if it is in one of the github actions cidr block", async () => {
+    // IPv4
+    expect(await isGithubActionsAddress("1.2.3.4")).toBe(true);
+    expect(await isGithubActionsAddress("5.6.7.252")).toBe(true);
+    expect(await isGithubActionsAddress("5.6.7.69")).toBe(true);
+    // IPv6
+    expect(await isGithubActionsAddress("1:2:3:4:5:6:7:8")).toBe(true);
+    expect(await isGithubActionsAddress("9:10:11:12:13:14:15:16")).toBe(true);
+    expect(await isGithubActionsAddress("9:10:11:12:13:ffff::")).toBe(true);
+  });
+
+  test("it should return false if it is not in one of the github actions cidr block", async () => {
+    // IPv4
+    expect(await isGithubActionsAddress("1.2.4.5")).toBe(false);
+    expect(await isGithubActionsAddress("5.7.8.9")).toBe(false);
+    // IPv6
+    expect(await isGithubActionsAddress("1:2:3:4:5:7:8:9")).toBe(false);
+    expect(await isGithubActionsAddress("9:10:12:23:34::")).toBe(false);
+  });
+
+  test("it should return false if it is not a valid ip address", async () => {
+    expect(await isGithubActionsAddress("actions")).toBe(false);
+    // IPv4
+    expect(await isGithubActionsAddress("1.2.3.4.5")).toBe(false);
+    expect(await isGithubActionsAddress("5.6.7.8.9")).toBe(false);
+    // IPv6
+    expect(await isGithubActionsAddress("1:2:3:4:5:6:7:8:9")).toBe(false);
+    expect(await isGithubActionsAddress("9:g:8::")).toBe(false);
   });
 })
 
--- functions/src/utils/githubUtils.ts
+++ functions/src/utils/githubUtils.ts
@@ -1,57 +1,30 @@
-const IPv4RegExp: RegExp = /(([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-2])\.){3}([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-2])/gi
-const IPv4CIDRRegExp: RegExp = /(([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-2])\.){3}([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-2])(\/([1-2][0-9]|3[0-2]|[0-9]))/gi
+import axios from 'axios';
+import { isInSubnet } from 'is-in-subnet';
 
-const IPv6RegExp: RegExp = /(([0-9a-fA-f]){0,4}:){7}([0-9a-fA-f]){0,4}/gi
-const IPv6CIDRRegExp: RegExp = /(([0-9a-fA-f]){0,4}:){1,7}([0-9a-fA-f]){0,4}(\/(12[0-8]|1[0-1][0-9]|[0-9]{1,2}))/gi
-
-/**
- * Made with the help of code from Edd Mann
- * https://github.com/eddmann
- * https://tech.mybuilder.com/determining-if-an-ipv4-address-is-within-a-cidr-range-in-javascript/
- */
-function ip4ToInt(ip: string): number {
-  return ip.split('.').reduce((int: number, oct: string) => (int << 8) + parseInt(oct, 10), 0) >>> 0;
+export type GithubMeta = {
+  verifiable_password_authentication: boolean,
+  ssh_key_fingerprints: {
+    SHA256_RSA: string,
+    SHA256_DSA: string,
+  },
+  hooks: string[],
+  web: string[],
+  api: string[],
+  git: string[],
+  packages: string[],
+  pages: string[],
+  importer: string[],
+  actions: string[],
+  dependabot: string[],
 }
 
-export function(ipv6):
-
-  function ip6ToInt(ip: string): BigInt {
-
-  return ip.split(':').reduce((int: BigInt, oct: string) => (int << 8) + parseInt(oct, 16), 0n);
-}
-
-/**
- * Made with the help of code from Edd Mann
- * https://github.com/eddmann
- * https://tech.mybuilder.com/determining-if-an-ipv4-address-is-within-a-cidr-range-in-javascript/
- */
-export function isIpInCIDR(ip: string, cidr: string): boolean {
-  const IPv4Match = ip.match(IPv4RegExp);
-  const IPv4CIDRMatch = cidr.match(IPv4CIDRRegExp);
-  const IPv6Match = ip.match(IPv6RegExp);
-  const IPv6CIDRMatch = cidr.match(IPv6CIDRRegExp);
-  if ((IPv4Match === null || IPv4Match[0] !== ip
-    || IPv4CIDRMatch === null || IPv4CIDRMatch[0] !== cidr)
-    && (IPv6Match === null || IPv6Match[0] !== ip
-      || IPv6CIDRMatch === null || IPv6CIDRMatch[0] !== cidr)) {
-    return false;
+export async function isGithubActionsAddress(ip: string): Promise<boolean> {
+  const githubMeta: GithubMeta = JSON.parse((await axios.get('https://api.github.com/meta')).data);
+  try {
+    return isInSubnet(ip, githubMeta.actions);
+  } catch (e) {
+    console.error(`[isGithubActionAddress][ERROR]:\n\tParameters:\n\t\tip: ${ip}\n\tError: ${e.message}`);
+    return false
   }
-  if (IPv4Match !== null && IPv4Match[0] === ip) { // IPv4
-    const [range, sbits = '32'] = cidr.split('/');
-    const bits = parseInt(sbits, 10);
-    const mask = ~(2 ** (32 - bits) - 1);
-    return (ip4ToInt(ip) & mask) === (ip4ToInt(range) & mask);
-  } else { // IPv6
-    const [range, sbits = '128'] = cidr.split('/');
-    range.match('::');
-    if (range.match('::') !== null) {
-
-    }
-    const bits = parseInt(sbits, 10);
-    const mask = ~(2 ** (128 - bits) - 1);
-    return
-  }
-
-
 }
 
