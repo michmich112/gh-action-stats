0f89fc9cc9b250383a0b2866db1aa8ccee72d26f
michmich112 <cantacuzene.michel@gmail.com>
2021-09-12 23:22:37 -0700

    WIP IPv6

--- functions/src/__test__/utils/githubUtils.test.ts
+++ functions/src/__test__/utils/githubUtils.test.ts
@@ -16,14 +16,6 @@ describe("isIpInCIDR", () => {
     expect(isIpInCIDR("252.252.252.252", "253.0.0.0/8")).toBe(false);
   });
 
-  test("[IPv6] it should return true if ip is in CIDR block", () => {
-    const ip1 = "2603:1030:401:160:1:1";
-    const ip2 = "2603:1030:401:160:1111:1111";
-    const cidr = "2603:1030:401:160::/61";
-    expect(isIpInCIDR(ip1, cidr)).toBe(true);
-    expect(isIpInCIDR(ip2, cidr)).toBe(true);
-  });
-
   test("[IPv4] it should return false if ip is not in CIDR block", () => {
     const ip1 = "192.168.0.2";
     const ip2 = "192.168.1.1";
@@ -31,12 +23,28 @@ describe("isIpInCIDR", () => {
     expect(isIpInCIDR(ip1, cidr)).toBe(false);
     expect(isIpInCIDR(ip2, cidr)).toBe(false);
   });
+
+  test("[IPv6] it should return true if ip is in CIDR block", () => {
+    const cidr = "2603:1030:401:160::/61";
+    expect(isIpInCIDR("2603:1030:401:160:1:1:1:1", cidr)).toBe(true);
+    expect(isIpInCIDR("2603:1030:401:160:1111:1111:1111:1111", cidr)).toBe(true);
+    expect(isIpInCIDR("1111:2222:3333:4444:5555:6666:7777:8888", "1111:2222:3333:4444:5555:6666:7777:8888/128")).toBe(true);
+    expect(isIpInCIDR("1:2:3:4:5:6:7:8", "::/0")).toBe(true);
+  });
+
+  test("[IPv6] it should return false if an incorrect IPv6 CIDR block is passed", () => {
+    expect(isIpInCIDR("1:2:3:4:5:6:7:8", "2603:1030:401:160::/129")).toBe(false);
+    //expect(isIpInCIDR("1:2:3:4:5:6:7:8", ":/0")).toBe(false);
+  });
+
   test("[IPv6] it should return false if ip is not in CIDR block", () => {
     expect(false).toBe(true);
   });
+
   test("it should return false if comparing a IPv4 Address to an IPv6 CIDR block", () => {
     expect(false).toBe(true);
   });
+
   test("it should return false if comparing a IPv6 Address to an IPv4 CIDR block", () => {
     expect(false).toBe(true);
   });
--- functions/src/utils/githubUtils.ts
+++ functions/src/utils/githubUtils.ts
@@ -1,8 +1,8 @@
 const IPv4RegExp: RegExp = /(([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-2])\.){3}([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-2])/gi
 const IPv4CIDRRegExp: RegExp = /(([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-2])\.){3}([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-2])(\/([1-2][0-9]|3[0-2]|[0-9]))/gi
 
-// const IPv6RegExp: RegExp = /(([0-9a-fA-f]){0,4}:){7}([0-9a-fA-f]){0,4}/gi
-// const IPv6CIDRRegExp: RegExp = //g
+const IPv6RegExp: RegExp = /(([0-9a-fA-f]){0,4}:){7}([0-9a-fA-f]){0,4}/gi
+const IPv6CIDRRegExp: RegExp = /(([0-9a-fA-f]){0,4}:){1,7}([0-9a-fA-f]){0,4}(\/(12[0-8]|1[0-1][0-9]|[0-9]{1,2}))/gi
 
 /**
  * Made with the help of code from Edd Mann
@@ -21,7 +21,12 @@ function ip4ToInt(ip: string): number {
 export function isIpInCIDR(ip: string, cidr: string): boolean {
   const IPv4Match = ip.match(IPv4RegExp);
   const IPv4CIDRMatch = cidr.match(IPv4CIDRRegExp);
-  if (IPv4Match === null || IPv4Match[0] !== ip || IPv4CIDRMatch === null || IPv4CIDRMatch[0] !== cidr) {
+  const IPv6Match = ip.match(IPv6RegExp);
+  const IPv6CIDRMatch = cidr.match(IPv6CIDRRegExp);
+  if ((IPv4Match === null || IPv4Match[0] !== ip
+    || IPv4CIDRMatch === null || IPv4CIDRMatch[0] !== cidr)
+    && (IPv6Match === null || IPv6Match[0] !== ip
+      || IPv6CIDRMatch === null || IPv6CIDRMatch[0] !== cidr)) {
     return false;
   }
 
