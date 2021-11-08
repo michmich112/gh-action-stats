bd76c66f3e60b82326ffc798ab1a81ea6b13b059
michmich112 <cantacuzene.michel@gmail.com>
2021-11-07 22:34:28 -0800

    setup authentication persistance

--- src/components/GitHubAuth.svelte
+++ src/components/GitHubAuth.svelte
@@ -1,45 +1,50 @@
 <script lang="ts">
   import { auth, functions } from "../config/firebase.config";
-  import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+  import {
+    browserLocalPersistence,
+    GithubAuthProvider,
+    setPersistence,
+    signInWithPopup,
+  } from "firebase/auth";
   import { httpsCallable } from "firebase/functions";
   import { appStore, userAuthStore } from "../store";
   import { navigate } from "svelte-routing";
 
   const GithubProvider = new GithubAuthProvider();
   GithubProvider.addScope("read:user");
-  function signIn() {
-    appStore.update((s) => ({ ...s, isLoading: true }));
-    signInWithPopup(auth, GithubProvider)
-      .then((result) => {
-        const credential = GithubAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        const user = result.user;
 
-        const loginUser = httpsCallable(functions, "loginUser");
-        loginUser({ GithubToken: token }).then((res) => {
-          const { data } = res as any;
-          if (data.code > 201 || !data.username) {
-            alert("Unable to authenticate. Try again.");
-          } else {
-            userAuthStore.set({
-              authenticated: true,
-              userId: user.uid,
-              github: {
-                username: data.username,
-                email: data.email,
-                token: credential.accessToken,
-              },
-            });
-            navigate(`/dash/${data.username}`);
-          }
-          appStore.update((s) => ({ ...s, isLoading: false }));
+  async function signIn() {
+    appStore.update((s) => ({ ...s, isLoading: true }));
+    await setPersistence(auth, browserLocalPersistence);
+    try {
+      const result = await signInWithPopup(auth, GithubProvider);
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+
+      const loginUser = httpsCallable(functions, "loginUser");
+      const res = await loginUser({ GithubToken: token });
+      const { data } = res as any;
+      if (data.code > 201 || !data.username) {
+        alert("Unable to authenticate. Try again.");
+      } else {
+        userAuthStore.set({
+          authenticated: true,
+          userId: user.uid,
+          github: {
+            username: data.username,
+            email: data.email,
+            token: credential.accessToken,
+          },
         });
-      })
-      .catch((error) => {
-        console.error(`Code ${error.code}, Message ${error.message}`);
-        const credential = GithubAuthProvider.credentialFromError(error);
-        console.error("credential err", credential);
-      });
+        navigate(`/dash/${data.username}`);
+        appStore.update((s) => ({ ...s, isLoading: false }));
+      }
+    } catch (error) {
+      console.error(`Code ${error.code}, Message ${error.message}`);
+      const credential = GithubAuthProvider.credentialFromError(error);
+      console.error("credential err", credential);
+    }
   }
 </script>
 
