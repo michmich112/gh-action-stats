9f669913d6e5197246798d1773b9ae0d9a813eb4
michmich112 <cantacuzene.michel@gmail.com>
2021-12-06 18:44:53 -0800

    updated routing & pages to have proper landing page and proper routing

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { UserAuthStore, userAuthStore } from "./store";
   import Loader from "./components/Loader.svelte";
   import Header from "./components/Header.svelte";
+  import Landing from "./pages/Landing.svelte";
   import Authentication from "./pages/Authentication.svelte";
   import Dashboard from "./pages/Dashboard.svelte";
   import ActionData from "./pages/ActionData.svelte";
@@ -25,19 +26,23 @@
 <main>
   <Router {url}>
     <Route path="/get-started" component={GetStarted} />
-    {#if !authenticated}
-      <Route path="/error/:id" let:params><Error code={params.id} /></Route>
-      <Route component={Authentication} />
-    {:else}
-      <Route path="/error/:id" let:params><Error code={params.id} /></Route>
+    <Route path="/error/:id" let:params><Error code={params.id} /></Route>
+    <Route path="/auth" component={Authentication} />
+    <Route path="/error/:id" let:params><Error code={params.id} /></Route>
+    {#if authenticated}
       <Route path="/action/:userId/:actionName" let:params>
         <ActionData
           actionCreator={params.userId}
           actionName={params.actionName}
-        /></Route
-      >
+        />
+      </Route>
       <Route path="/dash/{username}" component={Dashboard} />
-      <Route component={Dashboard} />
+      <Route path="/" component={Landing} />
+      <Route path="*"><Error code="404" /></Route>
+    {/if}
+    {#if !authenticated}
+      <Route path="/" component={Landing} />
+      <Route path="*" component={Authentication} />
     {/if}
   </Router>
 </main>
--- src/pages/Authentication.svelte
+++ src/pages/Authentication.svelte
@@ -1,23 +1,20 @@
 <script lang="ts">
   import GitHubAuth from "../components/GitHubAuth.svelte";
   import { navigate } from "svelte-routing";
+  import { UserAuthStore, userAuthStore } from "../store";
+
+  let authenticated: boolean = false;
+  let username: string = "";
+
+  userAuthStore.subscribe((userAuth: UserAuthStore) => {
+    authenticated = userAuth.authenticated;
+    if (authenticated) username = userAuth.github.username;
+  });
+
+  if (authenticated) navigate(`/dash/${username}`);
 </script>
 
 <div class="main">
-  <div class="horizontal-grid">
-    <div>
-      <h1>Get detailed analytics from your GitHub actions.</h1>
-      <h2>
-        Know who uses your action, errors, and more to make your action better
-        for all
-      </h2>
-    </div>
-    <img
-      class="half-width"
-      src="assets/images/Screen_Shot_2021-12-04_at_11.32.08_PM.png"
-      alt="Actions Dashboard Screenshot"
-    />
-  </div>
   <div class="vertical-grid">
     <GitHubAuth />
     <h1>Or</h1>
@@ -26,30 +23,6 @@
 </div>
 
 <style>
-  .half-width {
-    width: 45%;
-  }
-
-  .horizontal-grid {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    text-align: start;
-  }
-  @media only screen and (max-width: 500px) {
-    .half-width {
-      width: 85%;
-    }
-    .horizontal-grid {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      text-align: center;
-    }
-  }
-
   .vertical-grid {
     display: flex;
     flex-direction: column;
