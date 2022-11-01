a84ce0c92a5243b3ef246cc9487a6b3842d5dfb0
michmich112 <cantacuzene.michel@gmail.com>
2022-11-01 03:43:49 -0700

    Added Supabase client and documentation

---
+++ functions/src/infrastructure/supabase/SupabaseClient.ts
@@ -0,0 +1,29 @@
+import {
+  createClient as createSupabaseClient,
+  SupabaseClient,
+} from "@supabase/supabase-js";
+
+let client: null | SupabaseClient;
+
+export function createClient(): SupabaseClient {
+  const { SUPABASE_URL, SUPABASE_KEY } = process.env;
+  if (SUPABASE_URL === undefined || !SUPABASE_URL) {
+    console.error(
+      "Unable to initialize Supabase Client due to missin SUPABASE_URL environment variable"
+    );
+    throw new Error("Missing SUPABASE_URL environment variable");
+  }
+
+  if (SUPABASE_KEY === undefined || !SUPABASE_KEY) {
+    console.error(
+      "Unable to initialize Supabase Client due to missin SUPABASE_KEY environment variable"
+    );
+    throw new Error("Missing SUPABASE_KEY environment variable");
+  }
+
+  if (!client) {
+    client = createSupabaseClient(SUPABASE_URL, SUPABASE_KEY);
+  }
+
+  return client;
+}
