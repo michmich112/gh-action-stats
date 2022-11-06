dd5e5906b023b579fff38ce46ff7d5255c938e5c
michmich112 <cantacuzene.michel@gmail.com>
2022-11-05 22:43:00 -0700

    added test implementation for storage [put, get, exists]

--- functions/src/__test__/infrastructure/supabase/SupabaseBadgeStorage.test.ts
+++ functions/src/__test__/infrastructure/supabase/SupabaseBadgeStorage.test.ts
@@ -21,6 +21,9 @@ function skip(client: any, repo: any, soft: boolean = false): boolean {
 describe(`${eut} tests`, () => {
   let client: null | SupabaseClient = null;
   let repo: null | BadgeStorage = null;
+  const svg = `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="50" cy="50" r="50" />
+  </svg>`;
 
   beforeAll(async () => {
     dotenv.config();
@@ -42,15 +45,137 @@ describe(`${eut} tests`, () => {
     }
   });
 
-  test("put", () => {
+  beforeEach(async () => {
     skip(client, repo);
+    // delete all from the bucket if there are files
+    const { error } = await client!.storage.emptyBucket(
+      repo?.bucketName ?? "badges"
+    );
+    if (error) {
+      console.warn(
+        `[${eut}] BeforeAll - error clearing storage bucket. Some tests might fail.`,
+        error
+      );
+    }
   });
 
-  test("get", () => {
-    skip(client, repo);
+  describe("put", () => {
+    test("it should succeed with any proper path", async function () {
+      skip(client, repo);
+
+      const { data: pre_data } = await client!.storage.from("badges").list();
+      await repo!.put("test_1.svg", svg); // test no path
+      await repo!.put("test/test_2.svg", svg); // test in single folder
+      await repo!.put("test/put/test_3.svg", svg); // test compound path
+      const { data: post_data } = await client!.storage.from("badges").list();
+
+      if (!pre_data || !post_data) throw new Error("Data Not Returned");
+      expect(post_data.length).toEqual(pre_data.length + 3);
+      const fileName = post_data.map((f) => f.name);
+      expect(fileName.includes("test_1.svg")).toBeTruthy();
+      expect(fileName.includes("test_2.svg")).toBeTruthy();
+      expect(fileName.includes("test_3.svg")).toBeTruthy();
+    });
+
+    test("it should fail if a non proper file path is passed", async function () {
+      skip(client, repo);
+      const { data: pre_data } = await client!.storage.from("badges").list();
+      let err = false;
+      try {
+        await repo!.put("test", svg); // test incorrect file name
+      } catch (e) {
+        err = true;
+      }
+
+      if (!err) throw new Error("Error was expected to be thrown");
+
+      const { data: post_data } = await client!.storage.from("badges").list();
+      if (!pre_data || !post_data) throw new Error("Data Not Returned");
+      expect(post_data.length).toEqual(pre_data.length);
+    });
   });
 
-  test("exists", () => {
-    skip(client, repo);
+  describe("get", () => {
+    describe("It should get an existing document if it exists", async function () {
+      test("It should get an existing document with no path", async function () {
+        skip(client, repo);
+        const path = "test.svg";
+        await repo!.put(path, svg); //test no path
+        const res = await repo!.get(path);
+        expect(res).toEqual(svg);
+      });
+      test("It should get an existing document with a single path", async function () {
+        skip(client, repo);
+        const path = "test/test.svg"; //test in single file
+        await repo!.put(path, svg);
+        const res = await repo!.get(path);
+        expect(res).toEqual(svg);
+      });
+      test("It should get an existing document with a compound path", async function () {
+        skip(client, repo);
+        const path = "test/get/test.svg"; //test in compound files
+        await repo!.put(path, svg);
+        const res = await repo!.get(path);
+        expect(res).toEqual(svg);
+      });
+    });
+    test("It should throw an error if the file doesn't exist", async function () {
+      skip(client, repo);
+      try {
+        await repo!.get("test/nonexistant/test.svg");
+      } catch {
+        return;
+      }
+      throw new Error(
+        "Expected an error to be thrown as the file does not exists"
+      );
+    });
+    test("It should throw an error if an invalid file path is passed", async function () {
+      skip(client, repo);
+      try {
+        await repo?.get("testing/bad_filename");
+      } catch {
+        return;
+      }
+      throw new Error(
+        "Expected an error to be thrown as an invalid filepath was passed"
+      );
+    });
+  });
+
+  describe("exists", () => {
+    describe("It should return true if the document exists", function () {
+      test("It should return true if the document exist with only file name", async function () {
+        skip(client, repo);
+        const path = "test.svg";
+        await repo!.put(path, svg);
+        const exist = await repo!.exists(path);
+        expect(exist).toBeTruthy();
+      });
+      test("It should return true if the document exists with file in single folder", async function () {
+        skip(client, repo);
+        const path = "exist/test.svg";
+        await repo!.put(path, svg);
+        const exist = await repo!.exists(path);
+        expect(exist).toBeTruthy();
+      });
+      test("It should return true if the document exist with file in compound folders", async function () {
+        skip(client, repo);
+        const path = "test/exist/test.svg";
+        await repo!.put(path, svg);
+        const exist = await repo!.exists(path);
+        expect(exist).toBeTruthy();
+      });
+    });
+    test("It should return false if the document does not exist", async function () {
+      skip(client, repo);
+      const exist = await repo!.exists("doesnt_exist.svg");
+      expect(exist).toBeFalsy();
+    });
+    test("It should not throw an error if an incorrect file path is passed", async function () {
+      skip(client, repo);
+      const exist = await repo!.exists("test/bad_filename");
+      expect(exist).toBeFalsy();
+    });
   });
 });
