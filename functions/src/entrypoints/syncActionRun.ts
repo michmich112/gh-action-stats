a285eff31af695097a42800ca0d9a21b396638d0
michmich112 <cantacuzene.michel@gmail.com>
2022-11-14 00:51:17 -0800

    updated sync badge operation

--- functions/src/entrypoints/syncActionRun.ts
+++ functions/src/entrypoints/syncActionRun.ts
@@ -1,13 +1,30 @@
+import axios from "axios";
 import * as functions from "firebase-functions";
 import ActionRun from "../domain/ActionRun.type";
 import ActionRunRepository from "../infrastructure/postgres/ActionRunRepository";
 import { createClient } from "../infrastructure/postgres/PostgresClient";
 
-async function SyncActionRun(message: functions.pubsub.Message, context: functions.EventContext) {
+async function SyncActionRun(
+  message: functions.pubsub.Message,
+  context: functions.EventContext
+) {
   const actionRun: ActionRun = message.json?.actionRun;
   if (actionRun === undefined) {
-    console.error("[SyncActionRun][BadMessage] - could not access actionRun data.");
+    console.error(
+      "[SyncActionRun][BadMessage] - could not access actionRun data."
+    );
     return;
+  }
+
+  try {
+    if (!process.env.V2_ENDPOINT) {
+      console.error("No V2_ENDPOINT env variable set");
+    } else {
+      const res = await axios.post(process.env.V2_ENDPOINT, actionRun);
+      console.log("V2 Sync return:", res);
+    }
+  } catch (e) {
+    console.error("Error synching run to V2", e);
   }
 
   const client = createClient();
@@ -16,9 +33,11 @@ async function SyncActionRun(message: functions.pubsub.Message, context: functio
     await client.connect(); // connect clinent
     const actionRunRepository = new ActionRunRepository(client);
     await actionRunRepository.create(actionRun);
-  } catch (e) {
+  } catch (e: any) {
     console.group();
-    console.error("[SyncActionRun] - Error connecting & adding an action run to the repository");
+    console.error(
+      "[SyncActionRun] - Error connecting & adding an action run to the repository"
+    );
     console.error(e.message);
     console.error(e.stack);
     console.groupEnd();
@@ -28,4 +47,6 @@ async function SyncActionRun(message: functions.pubsub.Message, context: functio
   }
 }
 
-export const syncActionRun = functions.pubsub.topic("action-runs").onPublish(SyncActionRun);
+export const syncActionRun = functions.pubsub
+  .topic("action-runs")
+  .onPublish(SyncActionRun);
